6a931607483dd204234cb4fa1f424489711c0b71
Smokyn <smokyn_cosmin@yahoo.com>
2020-08-13 15:07:19 +0300

    basic class structure

---
+++ classes/classes-basics.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  greet() {
+    console.log('Hello, my name is ' + this.name);
+  }
+}
+
+let person = new Person('Cosmin');
+person.greet();
