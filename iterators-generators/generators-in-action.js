efda752c22505a307dd2eb76f1d8c195b5b76d11
Smokyn <smokyn_cosmin@yahoo.com>
2020-08-14 14:50:16 +0300

    throw and return

--- iterators-generators/generators-in-action.js
+++ iterators-generators/generators-in-action.js
@@ -1,11 +1,15 @@
 function* gen(end) {
   for (let i = 0; i < end; i++) {
-    yield i;
+    try {
+      yield i;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
 let it = gen(2);
 console.log(it.next());
-console.log(it.next());
+console.log(it.return('O puscat'));
 console.log(it.next());
 console.log(it.next());
